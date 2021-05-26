1dbe60372ea775b400da29c88d622a8a8c3463ad
Che <cgoranov@gmail.com>
2021-05-25 21:06:09 -0500

    commit tree f13f818d91e11b55085fd6a3e12c8e15fc0d49bb

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,69 @@
+
+
 class FiguresController < ApplicationController
   # add controller methods
+
+  get '/figures/new' do
+    
+    erb :'/figures/new'
+
+  end
+
+  get '/figures' do
+    
+    @figures = Figure.all
+
+    erb :'/figures/index'
+
+  end
+
+  post '/figures' do
+
+    @figure = Figure.create(params['figure']) 
+
+    if !params[:title][:name].empty?
+      @title = Title.create(params[:name])
+      @figure.titles << @title
+    end
+    
+    if !params[:landmark][:name].empty? 
+      @landmark = Landmark.create(params[:landmark])
+      @figure.landmarks << @landmark
+    end
+
+    redirect to "/figures/#{@figure.id}"
+
+  end
+
+  get '/figures/:id/edit' do
+
+    @figure = Figure.find_by(id: params[:id])
+
+    @titles = Title.all
+
+    @landmarks = Landmark.all
+
+    erb :'/figures/edit'
+
+  end
+
+  patch '/figures/:id' do
+    
+    figure = Figure.find_by(id: params[:id])
+    figure.update(params[:figure])
+
+    if !params[landmark][name].empty?
+      figure.landmarks << Landmark.new(params[:landmark][:name])
+    end
+
+  end
+
+  get '/figures/:id' do
+  
+    @figure = Figure.find_by(id: params[:id])
+
+    erb :'/figures/show'
+
+  end
+
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,18 @@
 class LandmarksController < ApplicationController
   # add controller methods
+
+  get '/landmarks/new' do
+    @landmark = Landmark.find_by(id: params[:id])
+
+    erb :'landmarks/new'
+  end
+
+  post '/landmarks' do
+    
+    landmark = Landmark.create(params)
+
+    redirect to "landmarks/#{landmark.id}"
+
+  end
+
 end
